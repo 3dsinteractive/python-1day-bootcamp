b5357b3d4354887971f7b965d6104857cf7b49af
pong3ds <chaiyapong@3dsinteractive.com>
2025-04-06 12:25:49 +0700

    add 00 and 01

---
+++ 01-basic-operations/app.py
@@ -0,0 +1,69 @@
+"""
+This module demonstrates basic arithmetic operations in Python.
+"""
+
+def main():
+    """This function performs and prints the results of various arithmetic operations."""
+    print(1+1)
+    print(7/4)
+    print(7%4)
+
+    my_int: int = 10
+    print(my_int)
+    my_other_int = my_int
+    my_int = "450"
+    print(my_other_int)
+    print(my_int)
+
+    # my_plus_int = my_int + my_other_int
+    # print(my_plus_int)
+
+    my_dogs = ["Rex", "Fido"]
+    print(my_dogs)
+
+    print(type(my_dogs))
+
+    my_string = "Hello"
+    my_string = my_string + " World"
+    print(my_string)
+
+    name = "Alice"
+    greeting = f"Hello, {name}!"
+    print(greeting)
+
+    my_list = [1, 2, 3]
+    my_list[0] = 0
+    print(my_list)
+
+    multi_type_list = [1, "Hello", 3.14]
+    print(multi_type_list)
+    print(len(multi_type_list))
+
+    first_list = [1, 2, 3]
+    second_list = [4, 5, 6]
+    combined_list = first_list + second_list
+    combined_list.append(7)
+    print(combined_list)
+
+    combined_list.pop()
+    print(combined_list)
+
+    combined_list2 = combined_list
+    combined_list2.append(8)
+    print(combined_list)
+
+    numbers = [3, 2, 1]
+    numbers.sort()
+    print(numbers)
+    numbers.reverse()
+    print(numbers)
+
+    my_dict = {"name": "Alice", "age": 30}
+    print(my_dict)
+    print(my_dict["name"])
+    my_dict["gender"] = "Female"
+    print(my_dict)
+
+
+if __name__ == "__main__":
+    main()
